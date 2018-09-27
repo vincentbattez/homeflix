e256d66048b3ca13ca0665a1d256d7ff095a6833
Vincent Battez <vincent.battez.pro@gmail.com>
2018-09-27 22:35:34 +0200

    intégration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,38 @@
         
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <title>Homeflix</title>
     </head>
-    <body>
-        <p>fdef</p>
+    <body style="background-image: url(series/vikings/wallpaper-vikings.jpg)">
+        <div class="pull-right">
+            <div class="serie">
+                <h1 class="serie__name">Vikings</h1>
+                <span class="serie__episode">Épisode 1</span>
+                <span class="serie__saison">Saison 3</span>
+            </div>
+            <div>
+                <button class="btn btn-primary">
+                    <img src="{{ asset('images/icon-play.svg') }}" alt="play">
+                    Regarder
+                </button>
+                <button class="btn btn-secondary">
+                    <img src="{{ asset('images/icon-change.svg') }}" alt="change">
+                    Épisodes
+                </button>
+            </div>
+        </div>
+
+        <nav class="sidebar">
+            <ul class="list">
+                <li class="item active">
+                    <img class="affiche" src="{{ asset('series/vikings/affiche-vikings.png') }}" alt="Vikings">
+                    <p class="restant">26 épisodes restant</p>
+                </li>
+                <li class="item">
+                    <img class="affiche" src="{{ asset('series/greys-anatomy/affiche-greys-anatomy.png') }}" alt="greys-anatomy">
+                    <p class="restant">110 épisodes restant</p>
+                </li>
+            </ul>
+        </nav>
     </body>
 </html>
